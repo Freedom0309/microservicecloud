6a1cbb15b0a45935d372a98ce78422462b67a8d2
liufeng <15332622786@163.com>
2019-06-03 22:41:06 +0800

    init project

---
+++ microservicecloud-api/src/main/java/com/freedom/entity/Dept.java
@@ -0,0 +1,12 @@
+package com.freedom.entity;
+
+import java.io.Serializable;
+
+
+public class Dept implements Serializable {
+
+    private long deptNo;
+    private String deptName;
+    private String dbSource;
+
+}
