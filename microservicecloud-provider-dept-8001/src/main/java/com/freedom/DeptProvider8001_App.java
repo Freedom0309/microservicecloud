5ce5673adb18689bd078a19d3228145da6762024
liufeng <15332622786@qq.com>
2019-06-05 18:17:17 +0800

    dept-8002

--- microservicecloud-provider-dept-8001/src/main/java/com/freedom/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/freedom/DeptProvider8001_App.java
@@ -9,8 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class DeptProvider8001_App {
-    public static void main( String[] args )
-    {
-        SpringApplication.run(DeptProvider8001_App.class);
+    public static void main( String[] args ){
+        SpringApplication.run(DeptProvider8001_App.class, args);
     }
 }
--- microservicecloud-provider-dept-8001/src/main/java/com/freedom/dao/DeptDao.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/freedom/dao/DeptDao.java
@@ -1,16 +1,21 @@
 package com.freedom.dao;
 
 import com.freedom.entity.Dept;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface DeptDao {
 
-    public boolean addDept(Dept dept);
+    @Insert(value = "insert into dept(dname, db_source) value (#{dname}, DATABASE())")
+    boolean addDept(Dept dept);
 
-    public Dept findById(Long id);
+    @Select(value = "select deptno,dname,db_source from dept where deptno = #{id}")
+    Dept findById(Long id);
 
-    public List<Dept> findAll();
+    @Select(value = "select deptno,dname,db_source from dept")
+    List<Dept> findAll();
 }
